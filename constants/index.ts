b317284df8b3cde94cce796d9da753e99384c82c
unknown <elmurodvkh@gmail.com>
2025-02-25 18:03:13 +0500

    global server api vaqtincha o'zgardi

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,9 @@
 // Local server manzili
 export const LOCAL_SERVER_URL = "http://localhost:5000/api";
 // Global server manzili
-export const GLOBAL_SERVER_URL = "https://api.uychinuts.uz/api";
+// export const GLOBAL_SERVER_URL = "https://api.uychinuts.uz/api";
+// Global server manzili
+export const GLOBAL_SERVER_URL = "https://nuts-server-ipwp.onrender.com/api";
 // Korxona manzili
 export const COMPANY_LOCATION = "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5597.179613188658!2d71.782603!3d41.019833!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDAxJzExLjQiTiA3McKwNDYnNTcuNCJF!5e1!3m2!1sen!2s!4v1738775062763!5m2!1sen!2s";
 // Korxona telefon raqami
